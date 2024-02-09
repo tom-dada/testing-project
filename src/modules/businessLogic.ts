55c2e86f6f50d277ba2d0cde2b3c28e493a8bb4a
Tommaso Dario Danti <tommasodanti@yahoo.it>
2024-02-09 14:04:55 +0100

    feat(addGiftCard): implemented addGiftCard function

--- src/modules/businessLogic.ts
+++ src/modules/businessLogic.ts
@@ -1,8 +1,13 @@
-import { orderType, personalDataType } from "../types/types";
+import { giftCardType, orderType, personalDataType } from "../types/types";
 
 export const newOrder = (data: personalDataType): orderType => {
-  return {
+  return ({
     personalData: data,
     giftCards: [],
-  };
+  });
+};
+
+export const addGiftCard = (order: orderType, giftCard: giftCardType): orderType => {
+  order.giftCards.push(giftCard);
+  return order;
 };

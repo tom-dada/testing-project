cc9bfc323ef4b4f862315054f93548b0f8853c85
Tommaso Dario Danti <tommasodanti@yahoo.it>
2024-02-09 12:52:24 +0100

    feat(newOrder.test): add missing test for 'codice fiscale' validation

--- src/tests/newOrder.test.ts
+++ src/tests/newOrder.test.ts
@@ -26,6 +26,12 @@ describe("newOrder tests", () => {
   it("'codice fiscale' is 16 characters long", () => {
     expect(order.personalData.codiceFiscale).toHaveLength(16);
   });
+  it("'codice fiscale' is a valid Italian fiscal code", () => {
+    // Regex taken from http://blog.marketto.it/2016/01/regex-validazione-codice-fiscale-con-omocodia/
+    expect(order.personalData.codiceFiscale).toMatch(
+      /^(?:[A-Z][AEIOU][AEIOUX]|[AEIOU]X{2}|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[15MR][\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[AC-EHLMPR-T][26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$/i
+    );
+  });
   it("'nome' is a string", () => {
     expect(typeof order.personalData.nome).toBe("string");
   });

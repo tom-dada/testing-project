9e7432206ee841d35a9aed0a9da459fab20a66ac
Tommaso Dario Danti <tommasodanti@yahoo.it>
2024-02-09 14:24:56 +0100

    feat: Add getAmount test

---
+++ src/tests/getAmount.test.ts
@@ -0,0 +1,8 @@
+import { getAmount } from "../modules/businessLogic";
+
+describe("getAmount tests", () => {
+  it("placeholder test", () => {
+    //TODO: implement this test
+    expect(false).toBe(true);
+  });
+});
